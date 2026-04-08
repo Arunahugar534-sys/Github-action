7493dd35261d4e9477afae04d526682c48e4d97f
Arunahugar534-sys <arunahugar48@gmail.com>
2026-04-09 00:24:03 +0530

    tf_cicd

--- self_host/main.tf
+++ self_host/main.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "web" {
     ami = "ami-0ea87431b78a82070"
     instance_type = "t2.micro"
     tags = {
-
-        Name = "web-server"
-
+        Name = "Web-server"
     }
 }
